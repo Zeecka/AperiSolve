c9bd101768be70f5569acb581056ecf52eab0651
Zeecka <alexgarrido@protonmail.com>
2023-03-22 16:07:03 +0100

    fix mac bin

--- web/static/install.sh
+++ web/static/install.sh
@@ -1,8 +1,28 @@
 #!/bin/sh
 # author: @Zeecka_
+# Modified for macOS by Eric Pan
 # Usage: aperisolve <file>
 
-cat << EOF > /usr/bin/aperisolve
+unameOut="$(uname -s)"
+case "${unameOut}" in
+    Darwin*)    echo 'Installing for macOS...' && cat << EOF > /usr/local/bin/aperisolve
+HOST="https://www.aperisolve.com"
+ARGC=\$#
+EXPECTED_ARGS=1
+
+if [ \$# -eq \$EXPECTED_ARGS ]
+then
+    P=\$(realpath \$1) # Get File Path Browser
+    REPHASH=\$(curl -s -F file=@\$P \$HOST/upload | jq .File | tr -d '"') # Upload and get hash
+    xdg-open \$HOST/\$REPHASH  # Open Browser
+else
+    echo "[?] Usage: aperisolve <file>"
+fi;
+EOF
+chmod +x /usr/local/bin/aperisolve
+echo "Aperi'Solve 🍉 is installed (/usr/local/bin/aperisolve) 🙂."
+echo "Usage: aperisolve <file>";;
+    *)          echo 'Installing for Linux...' cat << EOF > /usr/bin/aperisolve
 HOST="https://www.aperisolve.com"
 ARGC=\$#
 EXPECTED_ARGS=1
@@ -18,4 +38,5 @@ fi;
 EOF
 chmod +x /usr/bin/aperisolve
 echo "Aperi'Solve 🍉 is installed (/usr/bin/aperisolve) 🙂."
-echo "Usage: aperisolve <file>"
+echo "Usage: aperisolve <file>";;
+esac
