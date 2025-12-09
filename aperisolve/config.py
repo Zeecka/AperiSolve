b97a66d07267db45ab26c91511f01cdb103d67da
Philip Zimmermann <philip.zimmermann@hslu.ch>
2025-12-09 17:01:53 +0100

    Added PNG resize solver to workers and config

--- aperisolve/config.py
+++ aperisolve/config.py
@@ -3,6 +3,6 @@
 from pathlib import Path
 
 IMAGE_EXTENSIONS = [".png", ".jpg", ".jpeg", ".gif", ".bmp", ".webp", ".tiff"]
-WORKER_FILES = ["binwalk", "foremost", "steghide", "zsteg"]
+WORKER_FILES = ["binwalk", "foremost", "steghide", "zsteg", "image_resize"]
 
 RESULT_FOLDER = Path(__file__).parent.resolve() / "results"
