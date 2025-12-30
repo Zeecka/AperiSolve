474ad6208588f1cf54300f2f30331cd7464763a5
Zeecka <alexgarrido@protonmail.com>
2025-12-30 17:08:34 +0100

    fix error showing binwalk

--- aperisolve/analyzers/binwalk.py
+++ aperisolve/analyzers/binwalk.py
@@ -45,7 +45,7 @@ def analyze_binwalk(input_img: Path, output_dir: Path) -> None:
         if extracted_dir.exists():
             shutil.rmtree(extracted_dir)
 
-        if len(stderr) > 0:
+        if len(stderr) > 0 and not zip_exist:
             err: dict[str, dict[str, Any]] = {
                 "binwalk": {
                     "status": "error",
